080805a41c6b029d42a49c0350a59912ee98a616
mamemomonga <mamemomonga@gmail.com>
2016-09-03 21:00:17 +0900

    vimとlocale

--- fabfile.py
+++ fabfile.py
@@ -4,40 +4,27 @@
 from fabric.api import *
 from fabric.contrib.files import *
 
+# SSHのエラーを出力する
+import logging
+logging.getLogger('paramiko.transport').addHandler(logging.StreamHandler())
+
+# ssh_configを使用する
+env.use_ssh_config = True
+# env.ssh_config_path = "ssh_configのパス"
+
 @task
 def setup():
 
-	# SSH公開鍵を登録する
-	import os
-	sshdir='/home/'+env.user+'/.ssh'
-	if not exists(sshdir):
-		run('mkdir '+sshdir)
-		run('chmod 755 '+sshdir)
-		append( sshdir+'/authorized_keys', open( os.environ['HOME']+"/.ssh/id_rsa.pub" ).read().rstrip())
-		run('chmod 644 '+sshdir+'/authorized_keys')
-
-	# sudo を登録する
-	if not exists("/etc/sudoers.d/wheel"):
-		print "root password"
-
-		run("""
-su -c 'DEBIAN_FRONTEND=noninteractive apt-get install -y sudo && \\
-  echo "%(USER)s ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/wheel && \\
-  chmod 600 /etc/sudoers.d/wheel
-'""" % { 'USER' : env.user })
-
-	# sshd
-	comment('/etc/ssh/sshd_config','^PermitRootLogin without-password',char='# ',use_sudo=True)
-	append( '/etc/ssh/sshd_config','PermitRootLogin no',use_sudo=True)
-	append( '/etc/ssh/sshd_config','PasswordAuthentication no',use_sudo=True)
+	execute('sshd_set_pubkey')
+	execute('sudo_setup')
+	execute('sshd_config')
 
 	# ロケール
 	sudo("uname -a")
-	# uncomment('/etc/locale.gen','^# en_US.UTF-8 UTF-8',char='# ',use_sudo=True)
-	# uncomment('/etc/locale.gen','^# ja_JP.UTF-8 UTF-8',char='# ',use_sudo=True)
+	uncomment('/etc/locale.gen','^# en_US.UTF-8 UTF-8',char='# ',use_sudo=True)
+	uncomment('/etc/locale.gen','^# ja_JP.UTF-8 UTF-8',char='# ',use_sudo=True)
 	sudo('locale-gen')
-	# append('/etc/bash.bashrc','LC_MESSAGES=en_US.UTF-8',use_sudo=True)
-	sudo("update-locale LANG=C")
+	sudo('update-locale LANG=ja_JP.UTF-8 MESSAGES=C')
 
 	# アップデート
 	# libpam-systemd を入れればsshdからshutdownしても固まらない
@@ -53,6 +40,37 @@ su -c 'DEBIAN_FRONTEND=noninteractive apt-get install -y sudo && \\
 	# うまくいかないのでsshコマンド経由でreboot
 	# rebootコマンドは作り直す必要があるようだ
 	local("ssh %s@%s 'sudo reboot'; exit 0" % ( env.user, env.host_string) )
+
+@task
+def sudo_setup():
+	# sudo を登録する
+	if not exists("/etc/sudoers.d/wheel"):
+		print "root password"
+
+		run("""
+su -c 'DEBIAN_FRONTEND=noninteractive apt-get install -y sudo && \\
+  echo "%(USER)s ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/wheel && \\
+  chmod 600 /etc/sudoers.d/wheel
+'""" % { 'USER' : env.user })
+
+# ssh 公開鍵を登録する
+@task
+def sshd_set_pubkey():
+	import os
+	sshdir='/home/'+env.user+'/.ssh'
+	if not exists(sshdir):
+		run('mkdir '+sshdir)
+		run('chmod 755 '+sshdir)
+		append( sshdir+'/authorized_keys', open( os.environ['HOME']+"/.ssh/id_rsa.pub" ).read().rstrip())
+		run('chmod 644 '+sshdir+'/authorized_keys')
+
+@task
+def sshd_config():
+	# sshdの設定を変更する
+	comment('/etc/ssh/sshd_config','^PermitRootLogin without-password',char='# ',use_sudo=True)
+	append( '/etc/ssh/sshd_config','PermitRootLogin no',use_sudo=True)
+	append( '/etc/ssh/sshd_config','PasswordAuthentication no',use_sudo=True)
+	sudo( 'service ssh restart' )
 
 @task
 def dnssd():
@@ -72,11 +90,9 @@ def dnssd():
 
 @task
 def vim():
-    sudo('DEBIAN_FRONTEND=noninteractive aptitude install -y vim')
-
-    if not exists('/root/.vimrc'):
-        append('/root/.vimrc',"""
-syntax on
+	sudo('DEBIAN_FRONTEND=noninteractive aptitude install -y vim')
+	if not exists('/etc/skel/.vimrc'):
+		append('/etc/skel/.vimrc',"""syntax on
 set wildmenu
 set history=100
 set number
@@ -91,7 +107,13 @@ set fileencodings=iso-2022-jp,utf-8,cp932,euc-jp,ucs2le,ucs-2
 set fenc=utf-8
 set enc=utf-8
 """,use_sudo=True)
-
-    sudo('cp /root/.vimrc /etc/skel/.vimrc')
-    sudo('echo "3" | update-alternatives --config editor')
+ 	
+ 	if not exists('/root/.vimrc',use_sudo=True):
+ 		sudo('cp /etc/skel/.vimrc /root/.vimrc')
+ 
+ 	if not exists('/home/%s/.vimrc' % ( env.user ),use_sudo=True):
+ 		sudo('cp /etc/skel/.vimrc /home/%s/.vimrc' % ( env.user ))
+ 		sudo('chown %s /home/%s/.vimrc' % ( env.user, env.user ))
+ 
+	sudo('echo "3" | update-alternatives --config editor')
 
